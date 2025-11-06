b1236763d771a793b9a898fd1cb4cd9f672c1ac2
nanasemai <42890263+nanasemai@users.noreply.github.com>
2025-11-06 17:28:27 +0800

    fix: Makefile ccflags-y += -I

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -991,10 +991,9 @@ static int msm_routing_get_adm_topology(int fedai_id, int session_type,
 				cal_block->cal_info)->topology;
 			cal_utils_mark_cal_used(cal_block);
 		}
-		mutex_unlock(&cal_data[ADM_LSM_TOPOLOGY_CAL_TYPE_IDX]->lock);
-	}
+	mutex_unlock(&cal_data[ADM_LSM_TOPOLOGY_CAL_TYPE_IDX]->lock);
+}
 
-done:
 	pr_debug("%s: Using topology %d\n", __func__, topology);
 	return topology;
 }
